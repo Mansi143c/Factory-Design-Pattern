d3ae306b53db28107d05b88adfa11aee0df54eb8
Mansi143c <mansi143c@gmail.com>
2026-09-19 15:23:50 +0530

    Implement Factory Method pattern

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////////////Simple Factory////////////////////////////////////////////////
+
+IShoe s = ShoeFactory.GetShoe("Addidas");
+s.make();
+interface IShoe
+{
+    void make();
+}
+
+class Nike : IShoe
+{
+   public void make() => Console.WriteLine("Nike"); 
+}
+
+class Puma : IShoe
+{
+    public void make() => Console.WriteLine($"Puma");
+}
+
+class ShoeFactory
+{
+    public static IShoe GetShoe(string shoe)
+    {
+        if (shoe == "Nike")
+        {
+            return new Nike();
+        }
+        if (shoe == "Puma")
+        {
+            return new Puma();
+        }
+
+        throw new ArgumentException("Invalid shoe type");
+
+    }
+}
+
+
+
